042f715b9b15d825c19017d236f3624c336f12c0
user632 <90456511+user632@users.noreply.github.com>
2025-11-23 19:03:16 +0100

    ss

--- p6/lp.php
+++ p6/lp.php
@@ -4,11 +4,17 @@
 <style>
 .error {color: #FF0000;}
 body{justify-content: center;}
+.dane{
+          display: flex;
+      gap: 20px;
+      margin-top: 5px;
+      background-color: #30c902;
+}
 
 </style>
 </head>
 <body>  
-<div><?php echo htmlspecialchars($_POST['imie']); ?></div>
+<div class="dane"><?php echo htmlspecialchars($_POST['imie']); ?></div>
 <div><?php echo htmlspecialchars($_POST['nazw']); ?></div>
 <div><?php echo htmlspecialchars($_POST['ur']); ?></div>
 <div><?php echo htmlspecialchars($_POST['pass']); ?></div>
