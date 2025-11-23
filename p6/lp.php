54a9bbf42ee6f9257d2c06d148924048ad4e5420
user632 <90456511+user632@users.noreply.github.com>
2025-11-23 18:52:13 +0100

    ss

--- p6/lp.php
+++ p6/lp.php
@@ -3,10 +3,15 @@
 <head>
 <style>
 .error {color: #FF0000;}
+body{justify-content: center;}
+
 </style>
 </head>
 <body>  
-
+<div><?php echo htmlspecialchars($_POST['imie']); ?></div>
+<div><?php echo htmlspecialchars($_POST['nazw']); ?></div>
+<div><?php echo htmlspecialchars($_POST['ur']); ?></div>
+<div><?php echo htmlspecialchars($_POST['pass']); ?></div>
 Hi <?php echo htmlspecialchars($_POST['imie']); ?>.
 
 </body>
