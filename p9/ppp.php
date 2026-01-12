8b4c99acdbb0878a0978b35b475584ac9581de4b
user632 <90456511+user632@users.noreply.github.com>
2026-01-12 11:18:08 +0100

    ss

--- p9/ppp.php
+++ p9/ppp.php
@@ -1,9 +1,17 @@
 <?php
 
 function aaa($url){
-document=
-const collection = document.getElementsByClassName("example");
+$html = @file_get_contents($url);
+if ($html === false) return;
 
+
+$dom = new DOMDocument();
+@$dom->loadHTML($html);
+
+
+foreach ($dom->getElementsByClassName('sw1') as $link) {
+$href = $link->getAttribute('href');
+echo $href;
 }
 function crawl($url, $depth = 1, $maxDepth = 2, &$visited = []) {
 if ($depth > $maxDepth || in_array($url, $visited)) {
@@ -35,5 +43,5 @@ crawl($href, $depth + 1, $maxDepth, $visited);
 }
 
 
-crawl("http://example.com");
+
 ?>
