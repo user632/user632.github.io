c3baa280b236df4cb93a88b411ceb72f68828eef
user632 <90456511+user632@users.noreply.github.com>
2026-01-12 11:11:09 +0100

    sss

--- p9/ppp.php
+++ p9/ppp.php
@@ -1,1 +1,33 @@
-ss
+<?php
+function crawl($url, $depth = 1, $maxDepth = 2, &$visited = []) {
+if ($depth > $maxDepth || in_array($url, $visited)) {
+return;
+}
+
+
+$visited[] = $url;
+echo "<b>Crawluję:</b> $url<br>";
+
+
+$html = @file_get_contents($url);
+if ($html === false) return;
+
+
+$dom = new DOMDocument();
+@$dom->loadHTML($html);
+
+
+foreach ($dom->getElementsByTagName('a') as $link) {
+$href = $link->getAttribute('href');
+
+
+if (!str_starts_with($href, 'http')) continue;
+
+sleep(1);
+crawl($href, $depth + 1, $maxDepth, $visited);
+}
+}
+
+
+crawl("http://example.com");
+?>
